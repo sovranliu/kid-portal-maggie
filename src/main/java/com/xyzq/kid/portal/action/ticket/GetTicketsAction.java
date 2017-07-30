d3e82f9688d43cbc7c86b99ea70dee7d046ee33c
meiyejun <meuyejun@126.com>
2017-07-30 20:45:54 +0800

    meiyejun

---
+++ src/main/java/com/xyzq/kid/portal/action/ticket/GetTicketsAction.java
@@ -0,0 +1,42 @@
+package com.xyzq.kid.portal.action.ticket;
+
+import com.xyzq.kid.common.action.CustomerAction;
+import com.xyzq.kid.logic.ticket.service.TicketService;
+import com.xyzq.simpson.base.json.JSONObject;
+import com.xyzq.simpson.maggie.access.spring.MaggieAction;
+import com.xyzq.simpson.maggie.framework.Context;
+import com.xyzq.simpson.maggie.framework.Visitor;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * 增票
+ */
+@MaggieAction(path = "kid/wechat/getTickets")
+public class GetTicketsAction extends CustomerAction{
+    /**
+     * Action中只支持Autowired注解引入SpringBean
+     */
+    @Autowired
+    private TicketService ticketService;
+
+
+    /**
+     * 动作执行
+     *
+     * @param visitor 访问者
+     * @param context 请求上下文
+     * @return 下一步动作，包括后缀名，null表示结束
+     */
+    @Override
+    public String execute(Visitor visitor, Context context) throws Exception {
+
+        String result = super.execute(visitor, context);
+        if(null != result) {
+            return result;
+        }
+        String mobileNo = (String) context.get(CONTEXT_KEY_MOBILENO);
+
+        context.set("data", JSONObject.convertFromObject(ticketService.getTicketsInfoByOwnerMobileNo(mobileNo)));
+        return "success.json";
+    }
+}
