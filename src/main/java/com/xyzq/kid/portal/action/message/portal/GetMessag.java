3c3bf17d0deef4ffecf25a45e13803850cfba09d
keyanggui <273927457@qq.com>
2017-08-06 19:36:23 +0800

    getMessage

--- src/main/java/com/xyzq/kid/portal/action/message/portal/GetMessag.java
+++ src/main/java/com/xyzq/kid/portal/action/message/portal/GetMessag.java
@@ -1,32 +1,42 @@
 package com.xyzq.kid.portal.action.message.portal;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
-import com.xyzq.kid.portal.action.user.portal.PortalUserAjaxAction;
+import com.google.gson.Gson;
 import com.xyzq.kid.logic.message.dao.po.Message;
 import com.xyzq.kid.logic.message.service.MessageService;
 import com.xyzq.kid.logic.user.entity.UserEntity;
 import com.xyzq.kid.logic.user.service.UserService;
+import com.xyzq.kid.portal.action.user.portal.PortalUserAjaxAction;
 import com.xyzq.simpson.maggie.access.spring.MaggieAction;
 import com.xyzq.simpson.maggie.framework.Context;
 import com.xyzq.simpson.maggie.framework.Visitor;
 
-@MaggieAction(path="kid/portal/getMessageReply")
-public class GetMessagReply extends PortalUserAjaxAction {
+@MaggieAction(path="kid/wechat/getMessage")
+public class GetMessag extends PortalUserAjaxAction {
 	
 	@Autowired
 	UserService userService;
 	
 	@Autowired
 	MessageService messageService;
-
+	
+	Gson gson=new Gson();
+	
 	@Override
 	public String doExecute(Visitor visitor, Context context) throws Exception {
 		String mobileNo=(String)context.get(CONTEXT_KEY_MOBILENO);
 		UserEntity user=userService.selectByMolieNo(mobileNo);
-		List<Message> msgLit=messageService.queryAllMessageByUserId(user.id);
+		Message msg=messageService.queryLatestAnswer(user.id);
+		Map<String,String> map=new HashMap<>();
+		if(msg!=null){
+			map.put("content", msg.getAnswer());
+		}
+		context.set("0", "0");
+		context.set("data", gson.toJson(map));
 		return "success.json";
 	}
 	
