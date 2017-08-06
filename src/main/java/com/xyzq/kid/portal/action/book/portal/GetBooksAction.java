0cd3f7e1ad940a3c7923a88967a814f9df1624cd
keyanggui <273927457@qq.com>
2017-08-06 18:04:03 +0800

    bug

--- src/main/java/com/xyzq/kid/portal/action/book/portal/GetBooksAction.java
+++ src/main/java/com/xyzq/kid/portal/action/book/portal/GetBooksAction.java
@@ -50,7 +50,7 @@ public class GetBooksAction extends PortalUserAjaxAction{
 		if(ticketList!=null&&ticketList.size()>0){
 			for(int i=0;i<=ticketList.size();i++){
 				TicketEntity ticket=ticketList.get(i);
-				Book book=bookService.queryBookRecByTicketId(Integer.valueOf(ticket.serialNumber));
+				Book book=bookService.queryBookRecByTicketId(Integer.valueOf(ticket.id));
 				if(book!=null){
 					Map<String,String> bookMap=new HashMap<>();
 					bookMap.put("id", String.valueOf(i));
