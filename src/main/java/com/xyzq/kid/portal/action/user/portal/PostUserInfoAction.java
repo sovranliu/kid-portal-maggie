2477c7377b24679a1c9dbd11a4028d92d42ba2c9
sovranliu <sovranliu@aliyun.com>
2017-08-06 13:05:09 +0800

    ......

--- src/main/java/com/xyzq/kid/portal/action/user/portal/PostUserInfoAction.java
+++ src/main/java/com/xyzq/kid/portal/action/user/portal/PostUserInfoAction.java
@@ -32,17 +32,14 @@ public class PostUserInfoAction extends PortalUserAjaxAction {
      */
     @Override
     public String doExecute(Visitor visitor, Context context) throws Exception {
-
         UserEntity userEntity = new UserEntity();
         userEntity.telephone = (String) context.get(CONTEXT_KEY_MOBILENO);
         userEntity.openid = (String) context.get(CONTEXT_KEY_OPENID);
         userEntity.userName = (String)context.parameter("userName");
-        userEntity.sex = (Integer)context.parameter("sex", -1);
-        userEntity.address = (String)context.parameter("address", "未填");
+        userEntity.sex = (Integer)context.parameter("sex", 0);
+        userEntity.address = (String)context.parameter("address", "");
         userEntity.subscribetime = CommonTool.dataToStringYMDHMS(new Date());
-
-        userService.insertSelective(userEntity);
+        userService.updateByMobileNo(userEntity);
         return "success.json";
     }
-
 }
