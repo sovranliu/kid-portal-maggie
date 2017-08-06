b941d4b576008225bbe924cece94549bea97326e
sovranliu <sovranliu@aliyun.com>
2017-08-06 14:43:20 +0800

    .......

--- src/main/java/com/xyzq/kid/portal/action/ticket/BuyTicketAction.java
+++ src/main/java/com/xyzq/kid/portal/action/ticket/BuyTicketAction.java
@@ -1,16 +1,16 @@
 package com.xyzq.kid.portal.action.ticket;
 
 import com.xyzq.kid.portal.action.user.portal.PortalUserAjaxAction;
-import com.xyzq.kid.logic.config.common.ConfigCommon;
 import com.xyzq.kid.logic.config.service.ConfigService;
-import com.xyzq.kid.logic.ticket.entity.TicketEntity;
 import com.xyzq.kid.logic.ticket.service.TicketService;
+import com.xyzq.simpson.base.etc.Base64;
+import com.xyzq.simpson.base.json.JSONObject;
+import com.xyzq.simpson.base.json.JSONString;
 import com.xyzq.simpson.maggie.access.spring.MaggieAction;
 import com.xyzq.simpson.maggie.framework.Context;
 import com.xyzq.simpson.maggie.framework.Visitor;
 import org.springframework.beans.factory.annotation.Autowired;
-
-import java.math.BigDecimal;
+import org.springframework.beans.factory.annotation.Value;
 
 /**
  * 范例动作
@@ -18,12 +18,15 @@ import java.math.BigDecimal;
 @MaggieAction(path = "kid/portal/buyTicket")
 public class BuyTicketAction extends PortalUserAjaxAction {
     /**
-     * Action中只支持Autowired注解引入SpringBean
+     * 微信购票页面地址
      */
-    @Autowired
-    private TicketService ticketService;
-    @Autowired
-    private ConfigService configService;
+    @Value("${KID.URL_PAGE_BUYTICKET_WECHAT}")
+    private String url_page_buyticket_wechat;
+    /**
+     * 微信购票页面地址
+     */
+    @Value("${KID.URL_IMAGE_QRCODE}")
+    private String url_image_qrcode;
 
 
     /**
@@ -35,36 +38,11 @@ public class BuyTicketAction extends PortalUserAjaxAction {
      */
     @Override
     public String doExecute(Visitor visitor, Context context) throws Exception {
-
-//        String result = super.execute(visitor, context);
-//        if(null != result) {
-//            return result;
-//        }
-//
-//        TicketEntity ticketEntity = new TicketEntity();
-//
-//        ticketEntity.ownermobileno = (String) context.get(CONTEXT_KEY_MOBILENO);
-//        ticketEntity.payeropenid = (String) context.get(CONTEXT_KEY_OPENID);
-//        String insurance = (String)context.parameter("needRefundInsurance");
-//        if("true".equals(insurance)) {
-//            ticketEntity.insurance = true;
-//        } else if("false".equals(insurance)) {
-//            ticketEntity.insurance = false;
-//        }
-//        //TODO 获取orderno
-//
-//        int type = (Integer)context.parameter("ticketType", -1);
-//        if(type == TicketEntity.TICKET_TYPE_SINGLE) {
-//            int price = Integer.parseInt(configService.load(ConfigCommon.FEE_SINGLETICKET));
-//            ticketEntity.price = new BigDecimal(price);
-//            ticketService.buySingleTickets(ticketEntity);
-//        } else if(type == TicketEntity.TICKET_TYPE_GROUP) {
-//            int price = Integer.parseInt(configService.load(ConfigCommon.FEE_GROUPTICKET));
-//            ticketEntity.price = new BigDecimal(price);
-//            int num = (Integer)context.parameter("ticketNum", -1);
-//            ticketService.buyGroupleTickets(ticketEntity, num);
-//        }
-
+        String url = url_page_buyticket_wechat + "?type=" + context.parameter("ticketType") + "&mobileNo=" + context.get(PortalUserAjaxAction.CONTEXT_KEY_MOBILENO);
+        String qrCodeUrl = url_image_qrcode + Base64.encode(url) + ".jpg";
+        JSONObject json = new JSONObject();
+        json.put("qrcode", new JSONString(qrCodeUrl));
+        context.set("data", json);
         return "success.json";
     }
 }
